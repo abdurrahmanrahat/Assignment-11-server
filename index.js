e8e278f6b318f33a299bcc0eb2bb6d7b83f9ffbc
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-05-22 22:09:47 +0600

    Get data from mongodb

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
     Code from mongoDB
 --------------------------*/
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mjja2r0.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mjja2r0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,6 +31,40 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        // collection in sportToysDB from MongoDB
+        const skateboardCollection = client.db('sportToysDB').collection('Skateboard');
+        const golfSetCollection = client.db('sportToysDB').collection('GolfSet');
+        const plasticToysCollection = client.db('sportToysDB').collection('PlasticToys');
+
+
+
+        // get data fom mongodb
+        app.get('/skateboardToys', async (req, res) => {
+            const cursor = skateboardCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/golfsetToys', async (req, res) => {
+            const cursor = golfSetCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/plasticToys', async (req, res) => {
+            const cursor = plasticToysCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
